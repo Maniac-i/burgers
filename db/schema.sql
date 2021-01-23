cddf12d1540cda6f14b99b1690d51dba9bd672f4
Joe Maniaci <joe.maniaci1@gmail.com>
2021-01-23 13:38:32 -0500

    created seeds.sql file with insert queries

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers {
+CREATE TABLE burgers (
   id INTEGER AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(255) NOT NULL,
-  devoured BOOLEAN NOT NULL
-};
+  devoured BOOLEAN DEFAULT false NOT NULL 
+);
+
+
 
