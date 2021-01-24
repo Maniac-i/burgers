a87b237cc113258dfabb18a5e2c125b6a06b54d8
Joe Maniaci <joe.maniaci1@gmail.com>
2021-01-24 16:46:49 -0500

    created burger.js with the burger model code

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm");
+
+const burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", (res) => {
+      cb(res);
+    });
+  },
+
+  create: function(val, cols, cb) {
+    orm.insertOne("burgers", val, cols, (res) => {
+      cb(res);
+    });
+  },
+
+  update: function(colValObj, condition, cb) {
+    orm.updateOne("burgers", condition, (res) => {
+      cb(res);
+    });
+  },
+}
